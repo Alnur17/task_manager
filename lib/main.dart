0d0403837bf7aa21809933afbe2cd78bc16dee0b
Alnur17 <mr.nur30@gmail.com>
2024-06-26 16:29:23 +0600

    routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/screen/onboarding/email_verification_screen.dart';
+import 'package:task_manager/screen/onboarding/login_screen.dart';
+import 'package:task_manager/screen/onboarding/pin_verification_screen.dart';
+import 'package:task_manager/screen/onboarding/registration_screen.dart';
+import 'package:task_manager/screen/onboarding/set_password_screen.dart';
 import 'package:task_manager/screen/onboarding/splash_screen.dart';
 
 void main() {
@@ -12,12 +17,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task Manager',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => const SplashScreen(),
+        '/login' : (context) => const LoginScreen(),
+        '/registration' : (context) => const RegistrationScreen(),
+        '/emailVerification' : (context) => const EmailVerificationScreen(),
+        '/pinVerification' : (context) => const PinVerificationScreen(),
+        '/setPassword' : (context) => const SetPasswordScreen(),
+      },
     );
   }
 }
