89dfdbaa06d5b27b51513d1bf2c1b551ba015746
Alnur17 <mr.nur30@gmail.com>
2024-07-02 12:13:40 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screen/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.light,
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/ui/screen/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager/ui/utils/assets_utils.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: SvgPicture.asset(
+              AssetsUtils.backgroundSVG,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Center(
+            child: SvgPicture.asset(
+              AssetsUtils.logoSVG,
+              width: 90,
+              fit: BoxFit.scaleDown,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
