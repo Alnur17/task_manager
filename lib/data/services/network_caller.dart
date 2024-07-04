a8924fc2ea5a60ab90228df3b08d8fc8b32a57dc
Alnur17 <mr.nur30@gmail.com>
2024-07-04 15:36:05 +0600

    sign up api integration has been done

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -7,12 +7,21 @@ import 'package:task_manager/data/models/network_response.dart';
 class NetworkCaller {
   Future<NetworkResponse> getRequest(String url) async {
     try {
-      Response response = await get(Uri.parse(url));
+      Response response = await get(
+        Uri.parse(url),
+      );
       if (response.statusCode == 200) {
         return NetworkResponse(
-            true, response.statusCode, jsonDecode(response.body));
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
       } else {
-        return NetworkResponse(false, response.statusCode, null);
+        return NetworkResponse(
+          false,
+          response.statusCode,
+          null,
+        );
       }
     } catch (e) {
       log(e.toString());
@@ -23,12 +32,25 @@ class NetworkCaller {
   Future<NetworkResponse> postRequest(
       String url, Map<String, dynamic> body) async {
     try {
-      Response response = await post(Uri.parse(url), body: jsonEncode(body));
+      Response response = await post(
+        Uri.parse(url),
+        headers: {'Content-type': 'application/json'},
+        body: jsonEncode(body),
+      );
+      log(response.statusCode.toString());
+      log(response.body);
       if (response.statusCode == 200) {
         return NetworkResponse(
-            true, response.statusCode, jsonDecode(response.body));
+          true,
+          response.statusCode,
+          jsonDecode(response.body),
+        );
       } else {
-        return NetworkResponse(false, response.statusCode, null);
+        return NetworkResponse(
+          false,
+          response.statusCode,
+          null,
+        );
       }
     } catch (e) {
       log(e.toString());
--- lib/ui/screens/auth/registration_screen.dart
+++ lib/ui/screens/auth/registration_screen.dart
@@ -1,120 +1,197 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/data/models/network_response.dart';
+import 'package:task_manager/data/services/network_caller.dart';
+import 'package:task_manager/data/utils/urls.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
 
-class RegistrationScreen extends StatelessWidget {
+class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _isSignUpProgress = false;
+
+  Future<void> userSignUp() async {
+    _isSignUpProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+
+    Map<String, dynamic> responseBody = {
+      "email": _emailTEController.text.trim(),
+      "firstName": _firstNameTEController.text.trim(),
+      "lastName": _lastNameTEController.text.trim(),
+      "mobile": _mobileTEController.text.trim(),
+      "password": _passwordTEController.text,
+      "photo": "",
+    };
+
+    final NetworkResponse response =
+        await NetworkCaller().postRequest(Urls.registration, responseBody);
+    _isSignUpProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+    if (response.isSuccess) {
+      _emailTEController.clear();
+      _firstNameTEController.clear();
+      _lastNameTEController.clear();
+      _mobileTEController.clear();
+      _passwordTEController.clear();
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Registration success')));
+      }
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Registration failed')));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Join With Us',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your email...';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                ),
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your first name...';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  hintText: 'First Name',
-                ),
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your last name...';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  hintText: 'Last Name',
-                ),
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your mobile...';
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                  hintText: 'Mobile',
-                ),
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your password...';
-                  }
-                  return null;
-                },
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Register'),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Row(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Form(
+              key: _formKey,
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Have an account?',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          letterSpacing: 0.5,
-                        ),
+                    'Join With Us',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Sing In',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _emailTEController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
                     ),
+                    validator: (value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Please enter your email...';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    controller: _firstNameTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'First Name',
+                    ),
+                    validator: (value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Please enter your first name...';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    controller: _lastNameTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Last Name',
+                    ),
+                    validator: (value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Please enter your last name...';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    controller: _mobileTEController,
+                    keyboardType: TextInputType.phone,
+                    decoration: const InputDecoration(
+                      hintText: 'Mobile',
+                    ),
+                    validator: (value) {
+                      if ((value?.isEmpty ?? true) || value!.length < 11) {
+                        return 'Please enter your valid mobile...';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    controller: _passwordTEController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                    ),
+                    validator: (value) {
+                      if ((value?.isEmpty ?? true) || value!.length <= 5) {
+                        return 'Please enter your password more than 6 letters...';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Visibility(
+                      visible: _isSignUpProgress == false,
+                      replacement: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (!_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          userSignUp();
+                        },
+                        child: const Text('Register'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Have an account?',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                              letterSpacing: 0.5,
+                            ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'Sing In',
+                          style:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
