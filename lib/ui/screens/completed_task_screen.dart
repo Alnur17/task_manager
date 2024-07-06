2690a30f508995cd60f385c87380ff3e5e23cc37
Alnur17 <mr.nur30@gmail.com>
2024-07-06 12:33:39 +0600

    code modification has been added

--- lib/ui/screens/completed_task_screen.dart
+++ lib/ui/screens/completed_task_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager/ui/widgets/task_list_title.dart';
 import 'package:task_manager/ui/widgets/user_profile_banner.dart';
 
 class CompletedTaskScreen extends StatelessWidget {
@@ -18,12 +17,8 @@ class CompletedTaskScreen extends StatelessWidget {
                   child: ListView.separated(
                     itemCount: 10,
                     itemBuilder: (context, index) {
-                      return const TaskListTile(
-                        title: 'This is the new task title',
-                        subTitle: 'This is the new task subtitle',
-                        date: '25.05.2024',
-                        taskStatus: 'Completed',
-                      );
+                      return const SizedBox();
+                      // return const TaskListTile();
                     },
                     separatorBuilder: (BuildContext context, int index) {
                       return const Divider(
