06eb9818befce2d304ce6b813d74d88120e10bc7
Alnur17 <mr.nur30@gmail.com>
2024-07-26 11:36:08 +0600

    api calling using controller has been done

--- lib/ui/screens/auth/email_verification_screen.dart
+++ lib/ui/screens/auth/email_verification_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager/data/models/network_response.dart';
-import 'package:task_manager/data/services/network_caller.dart';
-import 'package:task_manager/data/utils/urls.dart';
+import 'package:get/get.dart';
+import 'package:task_manager/ui/state_managers/email_verification_controller.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
-
-import 'otp_verification_screen.dart';
 
 class EmailVerificationScreen extends StatefulWidget {
   const EmailVerificationScreen({super.key});
@@ -16,44 +13,44 @@ class EmailVerificationScreen extends StatefulWidget {
 
 class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  bool _emailVerificationInProgress = false;
+  //bool _emailVerificationInProgress = false;
   final TextEditingController _emailTEController = TextEditingController();
 
-  Future<void> sentOtpToEmail() async {
-    _emailVerificationInProgress = true;
-    if (mounted) {
-      setState(() {});
-    }
-    final NetworkResponse response = await NetworkCaller().getRequest(
-      Urls.sentOtpToEmail(
-        _emailTEController.text.trim(),
-      ),
-    );
-    _emailVerificationInProgress = false;
-    if (mounted) {
-      setState(() {});
-    }
-    if (response.isSuccess) {
-      if (mounted) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => OtpVerificationScreen(
-              email: _emailTEController.text.trim(),
-            ),
-          ),
-        );
-      } else {
-        if (mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Email verification failed'),
-            ),
-          );
-        }
-      }
-    }
-  }
+  // Future<void> sentOtpToEmail() async {
+  //   _emailVerificationInProgress = true;
+  //   if (mounted) {
+  //     setState(() {});
+  //   }
+  //   final NetworkResponse response = await NetworkCaller().getRequest(
+  //     Urls.sentOtpToEmail(
+  //       _emailTEController.text.trim(),
+  //     ),
+  //   );
+  //   _emailVerificationInProgress = false;
+  //   if (mounted) {
+  //     setState(() {});
+  //   }
+  //   if (response.isSuccess) {
+  //     if (mounted) {
+  //       Navigator.push(
+  //         context,
+  //         MaterialPageRoute(
+  //           builder: (context) => OtpVerificationScreen(
+  //             email: _emailTEController.text.trim(),
+  //           ),
+  //         ),
+  //       );
+  //     } else {
+  //       if (mounted) {
+  //         ScaffoldMessenger.of(context).showSnackBar(
+  //           const SnackBar(
+  //             content: Text('Email verification failed'),
+  //           ),
+  //         );
+  //       }
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -94,19 +91,23 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
                 const SizedBox(height: 24),
                 SizedBox(
                   width: double.infinity,
-                  child: Visibility(
-                    visible: _emailVerificationInProgress == false,
-                    replacement:
-                        const Center(child: CircularProgressIndicator()),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (!_formKey.currentState!.validate()) {
-                          return;
-                        }
-                        sentOtpToEmail();
-                      },
-                      child: const Text('Send'),
-                    ),
+                  child: GetBuilder<EmailVerificationController>(
+                    builder: (emailVerification) {
+                      return Visibility(
+                        visible: emailVerification.emailVerificationInProgress == false,
+                        replacement:
+                            const Center(child: CircularProgressIndicator()),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (!_formKey.currentState!.validate()) {
+                              return;
+                            }
+                            emailVerification.sentOtpToEmail(_emailTEController.text.trim());
+                          },
+                          child: const Text('Send'),
+                        ),
+                      );
+                    }
                   ),
                 ),
                 const SizedBox(height: 16),
@@ -121,7 +122,7 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
                     ),
                     TextButton(
                       onPressed: () {
-                        Navigator.pop(context);
+                        Get.back();
                       },
                       child: Text(
                         'Sign In',
